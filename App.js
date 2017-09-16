9cad0c131b876a200d76ae4f99c97640f1d0f484
Sherry Yang <yangxiaoli1994@gmail.com>
2017-09-17 00:20:17 +0200

    Fixed the absolute postion problem

--- App.js
+++ App.js
@@ -12,32 +12,39 @@ import {
 export default class App extends React.Component {
   constructor () {
   super()
-  this.spinValue = new Animated.Value(0)
+  this.state= {
+   spinValue: new Animated.Value(0),
+   posValue: new Animated.ValueXY()
+  }
 }
    componentDidMount () {
   this.spin()
 }
     
 spin () {
-  this.spinValue.setValue(0)
+  this.state.spinValue.setValue(0)
   Animated.timing(
-    this.spinValue,
+    this.state.spinValue,
     {
       toValue: 1,
       duration: 500,  // change the the value from 0 to 1 in 400mm
       easing: Easing.lineary
+    },
+    this.state.posValue,                      // The value to drive
+    {
+      toValue: {x:20,y:40},                             // Animate to final value of 1
     }
   ).start()//.start(() => this.spin())
 } 
     
     render () {
-  const spin = this.spinValue.interpolate({
+  const spin = this.state.spinValue.interpolate({
     inputRange: [0, 1],
     outputRange: ['0deg', '360deg']
   })
   return (
     <View style={styles.container}>
-      <Animated.Image
+      <Animated.Image // warning!! maybe use Animated View
         style={{
           width: 32,
           height: 32,
@@ -53,7 +60,8 @@ spin () {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    position: 'absolute',
+    left:    30,
+    top:    40,
   }
 });
